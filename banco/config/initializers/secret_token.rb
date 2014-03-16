c5ee6dd412fbaab03d7b3276a9317b2baeca5fb7
DriellyLM <driellylm@driellylm-POSITIVO-MOBILE.(none)>
2014-03-16 15:56:28 -0300

    Criação do Projeto

---
+++ banco/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Banco::Application.config.secret_key_base = '479ebaae519b6df3acd3ced78e17b41d8d8c0278eda1d74c610cffd90d577b531baf5c78fd19bb50363c690cf17d905c67e1d05eb45f1f91a55e0c3f76f1dbe3'
